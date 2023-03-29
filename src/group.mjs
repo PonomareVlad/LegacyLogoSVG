46953c3e4ea15e441fd7ff16ad02e08169efbfc2
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-03-29 06:51:22 +0500

    Update for groups

--- src/group.mjs
+++ src/group.mjs
@@ -2,18 +2,20 @@ import config from "../config.json" assert {type: "json"};
 import {JSDOM, VirtualConsole} from "jsdom";
 import {writeFileSync} from "fs";
 
-const virtualConsole = new VirtualConsole();
-virtualConsole.on("error", () => []);
-
 const {
     url,
     selector,
-    joins = {},
-    segments = [],
+    tags = {},
+    logos = {},
     excluded = [],
+    priority = [],
+    categories = {},
+    collects = "other",
 } = config;
 
-const groups = {};
+const virtualConsole = new VirtualConsole();
+virtualConsole.on("error", () => []);
+
 const response = await fetch(url);
 const html = await response.text();
 const {document} = new JSDOM(html, {virtualConsole})?.window;
@@ -21,41 +23,81 @@ const {innerHTML} = document.querySelector(selector);
 const {
     props: {
         pageProps: {
-            tags = [],
-            logos = [],
-            categories = []
+            logos: logosArray = [],
         } = {}
     } = {}
 } = JSON.parse(innerHTML);
 
-logos.forEach((logo = {}) => {
-    let {categories = [], files = []} = logo;
-    files = files.map(file => file.replace('.svg', '.tgs'));
-    if (files.length > 1) files = files.filter(file => file.endsWith('-icon.tgs') || file.endsWith('-icon-alt.tgs'));
-    const category = categories.filter(category => !excluded.includes(category)).pop() || categories.pop() || 'other';
-    if (!groups[category]) groups[category] = [];
-    groups[category].push({...logo, files});
-})
+const groups = new Map();
 
-excluded.forEach(key => groups[key] = groups[key].filter((logo = {}) => {
-    const {tags = []} = logo;
-    const segment = segments.find(segment => tags.includes(segment));
-    if (!segment) return true;
-    if (!groups[segment]) groups[segment] = [];
-    groups[segment].push(logo);
-}))
+const getGroup = key => {
+    if (groups.has(key)) return groups.get(key);
+    const group = new Set();
+    groups.set(key, group);
+    return group;
+}
+const mergeGroup = (child, parent) => {
+    const group = getGroup(parent);
+    [...getGroup(child).values()].forEach(item => group.add(item));
+    groups.delete(child);
+}
+const prioritySorter = (a, b) => priority.indexOf(a) - priority.indexOf(b);
+const joinMapper = ([parent, children = []]) => children.map(child => [child, parent]);
 
-Object.entries(joins).forEach(([parent, childs = []]) => childs.forEach(child => {
-    if (!groups[child] || !Array.isArray(groups[child])) return;
-    if (!groups[parent]) groups[parent] = [];
-    groups[parent] = [...groups[parent], ...groups[child]];
-    delete groups[child];
-}))
+const joinedTags = Object.values(tags).flat();
+const joinedLogos = Object.values(logos).flat();
+const joinedCategories = Object.values(categories).flat();
+const tagsJoin = Object.fromEntries(Object.entries(tags).flatMap(joinMapper));
+const logosJoin = Object.fromEntries(Object.entries(logos).flatMap(joinMapper));
+const categoriesJoin = Object.fromEntries(Object.entries(categories).flatMap(joinMapper));
 
-Object.entries(groups).forEach(([key, group]) => groups[key] = group.flatMap(({files = []}) => files).sort());
+logosArray.map(logo => {
+    const {
+        shortname,
+        tags = [],
+        categories = [],
+    } = logo || {};
+    if (joinedLogos.includes(shortname)) return getGroup(logosJoin[shortname]).add(logo);
+    const filteredCategories = categories.filter(category => !excluded.includes(category));
+    if (filteredCategories.length) {
+        const targetCategories = [...new Set(filteredCategories.map(category => {
+            if (!joinedCategories.includes(category)) return category;
+            return categoriesJoin[category];
+        }))];
+        const hasPriority = targetCategories.filter(category => priority.includes(category));
+        if (hasPriority.length) {
+            const targetCategory = hasPriority.sort(prioritySorter).shift();
+            return getGroup(targetCategory).add(logo);
+        }
+        return getGroup(targetCategories.shift()).add(logo);
+    }
+    const targetTags = tags.filter(tag => joinedTags.includes(tag));
+    if (!targetTags.length) return getGroup(collects).add(logo);
+    const hasPriority = targetTags.filter(tag => priority.includes(tag));
+    if (hasPriority.length) {
+        const targetTag = hasPriority.sort(prioritySorter).shift();
+        return getGroup(targetTag).add(logo);
+    }
+    return getGroup(tagsJoin[targetTags.shift()]).add(logo);
+});
 
-const stats = Object.fromEntries(Object.entries(groups).map(([group, items]) => [group, items.length]).sort(([, a], [, b]) => b - a))
+Object.entries(categories).forEach(([parent, children]) => children.forEach(child => mergeGroup(child, parent)));
 
-writeFileSync('./groups.json', JSON.stringify(groups, null, 2));
+const countByGroupSorter = ([, a] = [], [, b] = []) => b - a;
+const countByGroupMapper = ([name, items = new Set()]) => [name, items.size];
 
-console.debug(stats);
+groups.forEach((group, key) => !group.size ? groups.delete(key) : null);
+
+const countByGroup = new Map([...groups.entries()].map(countByGroupMapper).sort(countByGroupSorter));
+const countTotal = [...countByGroup.values()].reduce((sum, count) => sum + count, 0);
+const countLogos = logosArray.length;
+
+[
+    [countByGroup],
+    ["Logos:", countLogos],
+    ["Total:", countTotal],
+].forEach(args => console.log(...args));
+
+const sets = Object.fromEntries([...groups.entries()].map(([name, items]) => [name, [...items.values()]]));
+
+writeFileSync('./groups.json', JSON.stringify(sets, null, 2));
