9f74d82497c750d0e7ae4d2011c6e1613ff65ca7
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-03-30 23:00:39 +0500

    Update

--- src/utils.mjs
+++ src/utils.mjs
@@ -171,7 +171,7 @@ export const getLinks = sets => {
 
 export const sendLinks = async sets => {
     const links = getLinks(sets);
-    const message = ["📦 Uploading sets:", links.join("\r\n\r\n")].join("\r\n");
+    const message = ["📦 Uploading sets:", links.join("\r\n\r\n")].join("\r\n\r\n");
     return bot.sendMessage(chat_id, message);
 }
 
